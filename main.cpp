89fcf2bf914ecf12bf46930710b8ad3bc218852e
kyung2 <lovelyhyunkyung@gmail.com>
2017-10-03 15:47:16 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+//using namespace std;
+#pragma warning (disable : 4996)
+int main() {
+	int test, a, b;
+	float result =0;
+		scanf("%d%d", &a, &b);
+		result =(float) a / b;
+		printf("%1f \n", result);
+		int tmp;
+		scanf("%d", &tmp);
+	}
