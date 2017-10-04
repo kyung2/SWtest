f1ce5403b47f32b504225ea57f394d2ce4732ffb
kyung2 <lovelyhyunkyung@gmail.com>
2017-10-04 17:53:53 +0900

    2741

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include <cstdio>
 //using namespace std;
 #pragma warning (disable : 4996)
+/*
+나누기 test 사칙연산
 int main() {
 	int test, a, b;
 	float result =0;
 		scanf("%d%d", &a, &b);
 		result =(float) a / b;
 		printf("%1f \n", result);
-		int tmp;
-		scanf("%d", &tmp);
+			}
+			
+	*/
+
+// 2741
+int main() {
+	int input;
+	scanf("%d", &input);
+	int i = 1;
+	while (input--) {
+		printf("%d\n", i);
+		i++;
 	}
+	
+}
